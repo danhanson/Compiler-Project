42ce5535e500e6b6d71cc76dc0d32b72c4922ba8
Daniel Hanson <danielghanson93@gmail.com>
2016-04-21 18:42:56 -0400

    implemented instantiation

--- src/typechecker/expressions/Instantiation.java
+++ src/typechecker/expressions/Instantiation.java
@@ -1,22 +1,46 @@
 package typechecker.expressions;
 
+import java.util.Optional;
+
 import parser.MiniJavaParser.ExpressionContext;
+import parser.MiniJavaParser.InstantiationContext;
 import typechecker.scope.ExecutionScope;
 import typechecker.types.Type;
+import typechecker.types.Class;
 
 public class Instantiation extends Expression {
 
-	public static Instantiation fromExpressionContext(ExpressionContext con, ExecutionScope scope){
-		return null;
+	private final String classId;
+	private Class type;
+
+	Instantiation(String classId, ExecutionScope scope) {
+		super(scope);
+		this.classId = classId;
 	}
 
-	Instantiation(ExecutionScope scope) {
-		super(scope);
+	public static Instantiation fromExpressionContext(ExpressionContext con, ExecutionScope scope){
+		InstantiationContext ic = (InstantiationContext) con;
+		return new Instantiation(ic.ID().getText(), scope);
 	}
 
 	@Override
 	public Type returnType() {
-		// TODO Auto-generated method stub
-		return null;
+		return type;
+	}
+
+	@Override
+	public boolean checkTypes() {
+		Optional<Type> opt = scope().resolveType(classId);
+		if(!opt.isPresent()){
+			System.err.println("No class "+classId+" found in scope.");
+			return false;
+		}
+		Type t = opt.get();
+		if(t instanceof Class){
+			type = (Class) t;
+			return true;
+		}
+		System.err.println("Type "+classId+" is not a class.");
+		return false;
 	}
 }
